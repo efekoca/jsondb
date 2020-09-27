7ee5aaf2dfb234271a8da478f1508d4aab160868
Efe Koca <52746944+efekoca@users.noreply.github.com>
2020-09-27 15:27:55 +0300

    Add files via upload

---
+++ jsondb.php
@@ -0,0 +1,326 @@
+<?php
+    namespace jsonDB;
+
+    class jsondbException extends \Exception{
+        public function errorMsg(){
+            return "An unexpected error has occurred: " . $this->getMessage();
+        }
+    }
+    class db{
+        private $file;
+        public function __construct($file){
+            $pattern = "/.+\.json/";
+            if(!preg_match($pattern, $file))
+                throw new jsondbException("Only json files can be used.");
+            $this->file = $file;
+            if(!file_exists($file)){
+                touch($file);
+                if(!file_put_contents($file, json_encode(array("available" => true))))
+                    throw new jsondbException("An unknown error has occurred.");
+            }
+        }
+        public function get($par = null){
+            $readFile = file_get_contents($this->file);
+            if(($readFile == "") or (empty($readFile)) or (!$readFile)){
+                throw new jsondbException("There was an error reading the {$this->file} file.");
+            }
+            $decoded = json_decode($readFile, true);
+            if((!is_array($decoded)) and (is_null($decoded)))
+                throw new jsondbException("{$this->file} file contains corrupt json type.");
+            elseif(is_null($par))
+                return $decoded;
+            else{
+                if(!empty($decoded[$par]))
+                    return $decoded[$par];
+                return null;
+            }
+        }
+        public function delete($par){
+            /**
+             * @return bool
+             */
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded !== false){
+                    if(empty($decoded[$par]))
+                        return false;
+                    unset($decoded[$par]);
+                    $encoded = json_encode($decoded);
+                    if($encoded !== false){
+                        if(file_put_contents($this->file, $encoded))
+                            return true;
+                        return false;
+                    }
+                    return false;
+                }
+                throw new jsondbException("{$this->file} file contains corrupt json type.");
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function set($par, $val, $multiple = false){
+            /**
+             * @return bool
+             */
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded !== false){
+                    if(($multiple === false) and (!is_array($val)))
+                        $decoded[$par] = $val;
+                    else{
+                        foreach($val as $key => $value){
+                            $decoded[$key] = $value;
+                        }
+                    }
+                    $encoded = json_encode($decoded);
+                    if($encoded === false)
+                        throw new jsondbException("Decode error occurred.");
+                    if(!file_put_contents($this->file, $encoded))
+                        throw new jsondbException("Set operation failed.");
+                    return true;
+                }else{
+                    throw new jsondbException("{$this->file} file contains corrupt json type.");
+                }
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function add($par, $count){
+            /**
+             * @return bool
+             * @param int $count
+             */
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                elseif(empty($decoded[$par]))
+                    return false;
+                elseif(!is_numeric($decoded[$par]))
+                    return false;
+                else{
+                    $decoded[$par] += $count;
+                    $encoded = json_encode($decoded);
+                    if($encoded !== false){
+                        if(file_put_contents($this->file, $encoded))
+                            return true;
+                        throw new jsondbException("Add operation failed.");
+                    }
+                    throw new jsondbException("Add operation failed.");
+                }
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function subtract($par, $count){
+            /**
+             * @return bool
+             * @param int $count
+             */
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                elseif(empty($decoded[$par]))
+                    return false;
+                elseif(!is_numeric($decoded[$par]))
+                    return false;
+                else{
+                    $decoded[$par] -= $count;
+                    $encoded = json_encode($decoded);
+                    if($encoded !== false){
+                        if(file_put_contents($this->file, $encoded))
+                            return true;
+                        throw new jsondbException("Subtract operation failed.");
+                    }
+                    throw new jsondbException("Subtract operation failed.");
+                }
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function deleteFile(){
+            /**
+             * @return bool
+             */
+            if(!unlink($this->file))
+                return false;
+            return true;
+        }
+        public function deleteContent(){
+            /**
+             * @return bool
+             */
+            if(!file_put_contents($this->file, json_encode(array("available" => true))))
+                throw new jsondbException("Could not delete contents of {$this->file} file.");
+            return true;
+        }
+        public function order(){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                if(asort($decoded))
+                    return $decoded;
+                return false;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function reverse(){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Order operation failed.");
+                if(arsort($decoded))
+                    return $decoded;
+                return false;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function regex($pattern){
+                if($readFile = file_get_contents($this->file)){
+                    $decoded = json_decode($readFile, true);
+                    if($decoded === false)
+                        throw new jsondbException("Decode error occurred.");
+                    $testRegex = @preg_match($pattern, "testString");
+                    if($testRegex === false){
+                        if((preg_last_error() != PREG_NO_ERROR) and (preg_last_error() == PREG_INTERNAL_ERROR)){
+                            throw new jsondbException("You submitted a wrong pattern.");
+                        }
+                    }
+                    $newArr = array_filter($decoded, function($item) use ($pattern){
+                        return preg_match($pattern, $item);
+                    });
+                    return count($newArr) > 0 ? $newArr : null;
+                }
+                throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function has($par){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                if(!empty($decoded[$par]))
+                    return true;
+                return false;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function update($key, $val){
+            /**
+             * @return bool
+             */
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded !== false){
+                    $decoded[$key] = $val;
+                    $encoded = json_encode($decoded);
+                    if($encoded === false)
+                        throw new jsondbException("Decode error occurred.");
+                    if(!file_put_contents($this->file, $encoded))
+                        throw new jsondbException("Update operation failed.");
+                    return true;
+                }else{
+                    throw new jsondbException("{$this->file} file contains corrupt json type.");
+                }
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function hasVal($par){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                $newArr = array_filter($decoded, function($item) use ($par){
+                    $pattern = "/^{$par}$/";
+                    return preg_match($pattern, $item);
+                });
+                return count($newArr) > 0 ? $newArr : null;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function search($delimiter){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                $newArr = array_filter($decoded, function($item) use ($delimiter){
+                    $pattern = "/.*{$delimiter}.*/";
+                    return preg_match($pattern, $item);
+                });
+                return count($newArr) > 0 ? $newArr : null;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function searchKey($delimiter){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                $newArr = array();
+                foreach($decoded as $key => $value){
+                    $pattern = "/.*{$delimiter}.*/";
+                    if(!preg_match($pattern, $key))
+                        continue;
+                    $newArr[$key] = $value;
+                }
+                return count($newArr) > 0 ? $newArr : null;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function getStringVals(){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                $newArr = array_filter($decoded, function($item){
+                    $pattern = "/[a-zA-Z]+/";
+                    return preg_match($pattern, $item);
+                });
+                return count($newArr) > 0 ? $newArr : null;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function getStringKeys(){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                $newArr = array();
+                foreach($decoded as $key => $value){
+                    $pattern = "/[a-zA-Z]+/";
+                    if(!preg_match($pattern, $key))
+                        continue;
+                    $newArr[$key] = $value;
+                }
+                return count($newArr) > 0 ? $newArr : null;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function getNumericKeys(){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                $newArr = array();
+                foreach($decoded as $key => $value){
+                    $pattern = "/[0-9]+/";
+                    if(!preg_match($pattern, $key))
+                        continue;
+                    $newArr[$key] = $value;
+                }
+                return count($newArr) > 0 ? $newArr : null;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+        public function getNumericVals(){
+            if($readFile = file_get_contents($this->file)){
+                $decoded = json_decode($readFile, true);
+                if($decoded === false)
+                    throw new jsondbException("Decode error occurred.");
+                $newArr = array_filter($decoded, function($item){
+                    $pattern = "/[0-9]+/";
+                    return preg_match($pattern, $item);
+                });
+                return count($newArr) > 0 ? $newArr : null;
+            }
+            throw new jsondbException("There was an error reading the {$this->file} file.");
+        }
+    }
+?>
---
+++ tests.php
@@ -0,0 +1,34 @@
+<?php
+    require("jsondb.php");
+    $database = new jsonDB\db("data.json"); # While calling the class, database file must be entered as parameter.
+    try{
+        if(!empty($database->get()["available"]))
+            echo("JSONDB is active.");
+        else
+            die("JSONDB error occurred.");
+        # In case of any error, all of the following will return an error code. For more information: https://www.php.net/manual/en/language.exceptions.php
+        # Important Notes:
+        #Decode will return an error code if any of the write, read operations fail.
+        var_dump($database->regex("/[a-zA-Z0-9\-\.\:\+\-\_\#\=\%\~\@]+/")); # Returns Array(...) or if not found null.
+        var_dump($database->getStringVals()); # Returns Array(...) (All string values.) or if not found null.
+        var_dump($database->getStringKeys()); # Returns Array(...) (All string keys.) or if not found null.
+        var_dump($database->getNumericVals()); # Returns Array(...) (All numeric values.) or if not found null.
+        var_dump($database->getNumericKeys()); # Returns Array(...) (All numeric keys.) or if not found null.
+        var_dump($database->search("a")); # Returns Array(...) (Searches for the specified parameter in values.) or if not found anything null.
+        var_dump($database->searchKey("a")); # Returns Array(...) (Searches for the specified parameter in keys.) or if not found anything null.
+        var_dump($database->hasVal(1)); # Returns true (Checks if the specified parameter is equal to any value.) or if not equal false;
+        var_dump($database->has(1)); # Returns true (Checks if the specified parameter is equal to any key.) or if not equal false;
+        var_dump($database->set("key", "val")); # Returns true if successful or error code if an error occurred. (Note: Multiple entries can be made if the $val parameter is specified as an array.However, for this, the third parameter must be true.) (IMPORTANT NOTE: When sending an array, it may produce unexpected results if you submit a missing key or value! If you don't send a key, a key will be assigned by default. For example 0.)
+        var_dump($database->delete("key")); # Returns true if successful or error code (If the specified key is not found, it returns false.) if an error occurred.
+        var_dump($database->get()); # Returns Array(["available"] => 1 ...) (If the parameter is not specified.) or if a parameter is specified and if it not found null.
+        var_dump($database->update("key", "val")); # Returns true if successful or error code if an error occurred. (Updates a value for the specified key.)
+        var_dump($database->add("key", "number")); # Returns true if successful (Adds the specified number to the value of the specified key.) or error code (If the specified key is not found or if the value of the specified key is not a number, it returns false.) if an error occurred.
+        var_dump($database->subtract("key", "number")); # Returns true if successful (Subtracts the specified number to the value of the specified key.) or error code (If the specified key is not found or if the value of the specified key is not a number, it returns false.) if an error occurred.
+        var_dump($database->deleteFile()); # Returns true if successful or false if error occurred. (Deletes the called database file.)
+        var_dump($database->deleteContent()); # Returns true if successful or false if error occurred. (Deletes all database content.)
+        var_dump($database->order()); # Returns Array(...) or error code (Returns false if there is an error in the sequence.) if an error occurred. (Sorts all database content from A to Z, 0 to 9.)
+        var_dump($database->reverse()); # Returns Array(...) or error code (Returns false if there is an error in the sequence.) if an error occurred. (Sorts all database content from Z to A, 9 to 0.)
+    }catch(jsonDB\jsondbExpection $e){
+        echo($e->errorMsg()); # If an error occurs...
+    }
+?>
